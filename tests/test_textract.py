e74e14f80087bd49474d1adb74a69495c0d19e3b
Will Thompson <will.k.t@gmail.com>
2019-12-05 17:08:05 -0600

    added scispcay dependency

--- tests/test_textract.py
+++ tests/test_textract.py
@@ -6,4 +6,4 @@ def test_morph_matcher(morph_matcher):
     nlp = spacy.load("en_core_web_sm", disable=['ner'])
     doc = nlp("FINDINGS: Neoplasm, NOS")
     matches = morph_matcher(doc)
-    print(matches)
+    assert len(matches) == 1
