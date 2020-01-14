2dcf6d792ce89f84e79f3788906da277fc7274e5
Will Thompson <will.k.t@gmail.com>
2020-01-14 10:53:28 -0600

    updates

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,6 +1,7 @@
 import pytest
 from starlette.testclient import TestClient
-from datetime import datetime
+from fastapi.encoders import jsonable_encoder
+from datetime import datetime as dt
 from abstractor.service.main import *
 
 client = TestClient(app)
@@ -18,31 +19,29 @@ def abstraction_schema() -> AbstractionSchema:
         namespace_type="Abstractor::AbstractorNamespace",
         namespace_id=281,
         abstractor_rule_type="value",
-        updated_at=datetime.strptime("2019-12-16 13:07:50", "%Y-%m-%d %H:%M:%S"),
+        updated_at=dt.strptime("2019-12-16 13:07:50", "%Y-%m-%d %H:%M:%S"),
     )
 
 
 @pytest.fixture(scope="session")
-def abstraction_schema() -> AbstractionSchema:
-    return AbstractionSchema(
-        abstractor_abstraction_schema_id=1184,
-        abstractor_abstraction_schema_uri="https://schemas/1184.json",
-        abstractor_abstraction_abstractor_suggestions_uri="https://abstractions/669/abstractor_suggestions.json",
-        abstractor_abstraction_id=669,
-        abstractor_abstraction_source_id=1374,
-        abstractor_subject_id=1355,
-        namespace_type="Abstractor::AbstractorNamespace",
-        namespace_id=281,
-        abstractor_rule_type="value",
-        updated_at=datetime.strptime("2019-12-16 13:07:50", "%Y-%m-%d %H:%M:%S"),
+def suggest_request(abstraction_schema) -> SuggestRequest:
+    return SuggestRequest(
+        source_id=156,
+        source_type="NoteStableIdentifier",
+        source_method="note_text",
+        abstractor_rules_uri="https://moomin.com/abstractor_rules.json",
+        text="Looks like metastatic carcinoma to me.",
+        abstractor_abstraction_schemas=[
+            abstraction_schema
+        ]
     )
 
 
-def test_multiple_suggest():
-    pass
-    # response=client.post(
-    #     "/multiple_suggest",
-    #     json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"},
-    # )
-    # assert response.status_code == 201
-    # assert response.json() == {}
+def test_multiple_suggest(suggest_request):
+    print(jsonable_encoder(suggest_request))
+    response=client.post(
+        "/multiple_suggest",
+        json=jsonable_encoder(suggest_request),
+    )
+    assert response.status_code == 201
+    assert response.json() is None
