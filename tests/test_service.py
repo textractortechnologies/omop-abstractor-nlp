7198e7f3efda13514396811b7b6c1c25366ff16b
Will Thompson <will.k.t@gmail.com>
2020-01-14 10:25:57 -0600

    updates

--- tests/test_service.py
+++ tests/test_service.py
@@ -1,17 +1,48 @@
+import pytest
 from starlette.testclient import TestClient
-
-from abstractor.service.main import app
+from datetime import datetime
+from abstractor.service.main import *
 
 client = TestClient(app)
 
 
-def test_post():
-    response = client.get("/")
-    response = client.post(
-        "/items/",
-        headers={"X-Token": "coneofsilence"},
-        json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"},
+@pytest.fixture(scope="session")
+def abstraction_schema() -> AbstractionSchema:
+    return AbstractionSchema(
+        abstractor_abstraction_schema_id=1184,
+        abstractor_abstraction_schema_uri="https://schemas/1184.json",
+        abstractor_abstraction_abstractor_suggestions_uri="https://abstractions/669/abstractor_suggestions.json",
+        abstractor_abstraction_id=669,
+        abstractor_abstraction_source_id=1374,
+        abstractor_subject_id=1355,
+        namespace_type="Abstractor::AbstractorNamespace",
+        namespace_id=281,
+        abstractor_rule_type="value",
+        updated_at=datetime.strptime("2019-12-16 13:07:50", "%Y-%m-%d %H:%M:%S"),
     )
-    assert response.status_code == 200
-    assert response.status_code == 200
-    assert response.json() == {"msg": "Hello World"}
+
+
+@pytest.fixture(scope="session")
+def abstraction_schema() -> AbstractionSchema:
+    return AbstractionSchema(
+        abstractor_abstraction_schema_id=1184,
+        abstractor_abstraction_schema_uri="https://schemas/1184.json",
+        abstractor_abstraction_abstractor_suggestions_uri="https://abstractions/669/abstractor_suggestions.json",
+        abstractor_abstraction_id=669,
+        abstractor_abstraction_source_id=1374,
+        abstractor_subject_id=1355,
+        namespace_type="Abstractor::AbstractorNamespace",
+        namespace_id=281,
+        abstractor_rule_type="value",
+        updated_at=datetime.strptime("2019-12-16 13:07:50", "%Y-%m-%d %H:%M:%S"),
+    )
+
+
+def test_multiple_suggest():
+    pass
+    # response=client.post(
+    #     "/multiple_suggest",
+    #     json={"id": "foobar", "title": "Foo Bar", "description": "The Foo Barters"},
+    # )
+    # assert response.status_code == 201
+    # assert response.json() == {}
